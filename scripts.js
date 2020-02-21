965ae1d52b24d661730c177cebd7471dd5e0e1ba
Peter Francis <francisp336@gmail.com>
2020-02-20 23:46:46 -0500

    guage

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+
+
+
 function strDict(input) {
   stringOut = "";
 
@@ -6,9 +9,9 @@ function strDict(input) {
   }
 
   else {
-    stringOut += "<ul>" //  class=\"list-group flush\">";
+    stringOut += "<ul>"; // class=\"list-group flush\"
     for (var key in input) {
-      stringOut += "<li>" + key + ": " + strDict(input[key]) + "</li>"; // class=\"list-group-item\">
+      stringOut += "<li>" + key + ": " + strDict(input[key]) + "</li>"; //  class=\"list-group-item\">
     }
     stringOut += "</ul>";
   }
@@ -23,10 +26,32 @@ function strDict(input) {
 
 function loadWeather(zipCode){
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode+ "&units=imperial&APPID=f617b6cb95e94d59d5cc345b892aaabf",function(json){
-      document.getElementById("raw_data").innerHTML = strDict(json);
-      icon = json["weather"][0]["icon"];
-      document.getElementById("icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      str = ""
+      for (var key in json) {
+        str += "<h4><u>" + key + "</u></h4>";
+        str += strDict(json[key]);
+      }
+
+      document.getElementById("raw_data").innerHTML = str;
+      document.getElementById("icon").src = "https://openweathermap.org/img/wn/" + json["weather"][0]["icon"] + "@2x.png"
       console.log(JSON.stringify(json));
+
+
+      // calculations
+      n = (json["weather"][0]["id"]);
+      num = (10/8) * (n == 800 ? 0 : n) / 10; // weatherDict[json["weather"][0]["id"]]
+      clouds = json["clouds"]["all"];
+      temp = json["main"]["feels_like"];
+      humidity = json["main"]["humidity"];
+
+      round = 1000;
+      grade = (1/round) * Math.round(round * ((3 * num / 8) + (3 * clouds / 8) + (3 * (100 - temp) / 16) + (humidity / 16)));
+
+      document.getElementById("grade").innerHTML = ("" + grade).substring(0,4) + "%";
+
+      var gauge = new Gauge(document.getElementById("gauge"));
+
+      gauge.value(grade/100);
 
     });
 }
