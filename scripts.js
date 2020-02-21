1ce98ded940a614335573ec70f53ac50f8ab2ff0
Peter Francis <francisp336@gmail.com>
2020-02-21 00:00:30 -0500

    advice

--- scripts.js
+++ scripts.js
@@ -44,10 +44,31 @@ function loadWeather(zipCode){
       temp = json["main"]["feels_like"];
       humidity = json["main"]["humidity"];
 
-      round = 1000;
-      grade = (1/round) * Math.round(round * ((3 * num / 8) + (3 * clouds / 8) + (3 * (100 - temp) / 16) + (humidity / 16)));
-
+      grade = ((3 * num / 8) + (3 * clouds / 8) + (3 * (100 - temp) / 16) + (humidity / 16));
+      console.log("grade: " + grade)
       document.getElementById("grade").innerHTML = ("" + grade).substring(0,4) + "%";
+      document.getElementById("place").innerHTML = json["name"];
+
+      advice = "";
+      if (grade < 20) {
+        advice = "Not looking too great for math today.";
+      }
+      else if (grade < 40) {
+        advice = "Mehhhh.....";
+      }
+      else if (grade < 60) {
+        advice = "That fair... maybe some claculus?";
+      }
+      else if (grade < 80) {
+        advice = "It's lokin mighty fine for math today!";
+      }
+      else if (grade < 100) {
+        advice = "Get inside right now and solve the 3n+1 problem!";
+      }
+      else {
+        advice = "This is probably a mistake. If not a volcano is probably erupting...";
+      }
+      document.getElementById("advice").innerHTML = advice;
 
       var gauge = new Gauge(document.getElementById("gauge"));
 
